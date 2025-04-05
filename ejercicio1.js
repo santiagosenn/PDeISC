976c35979e2e6e647dc2b76e7a9bac670b776b2a
Your Name <you@example.com>
2025-04-04 22:41:04 -0300

    Agrego carpeta ejercicios con archivos

---
+++ ejercicio1.js
@@ -0,0 +1,4 @@
+console.log("Hola mundo");
+console.log("Fin");
+
+//muestro por pantalla las lineas Hola mundo y fin
---
+++ ejercicio2.js
@@ -0,0 +1,6 @@
+console.log(4 + 5);
+console.log(3 - 6);
+console.log(2 * 7);
+console.log(20 / 4);
+
+//realizo y muestro varias operaciones

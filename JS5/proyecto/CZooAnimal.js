21faaf6bdca93976ac1f48ad3b8b6c43f9d099e1
santiagosenn <130767362+santiagosenn@users.noreply.github.com>
2025-07-10 22:01:15 -0300

    Delete JS5 directory

--- JS5/proyecto/CZooAnimal.js
+++
@@ -1,9 +0,0 @@
-export class CZooAnimal {
-    constructor(id, nombre, jaula, tipo, peso) {
-        this.IdAnimal = id;
-        this.nombre = nombre;
-        this.JaulaNumero = jaula;
-        this.IdTypeAnimal = tipo;
-        this.peso = peso;
-    }
-}

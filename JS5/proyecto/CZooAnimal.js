21f25964c75d0a09772a359bf23cd660e1774d5d
Santiago <sennsantiago7@gmail.com>
2025-07-10 21:16:21 -0300

    agrego el proyecto correspondiente

---
+++ JS5/proyecto/CZooAnimal.js
@@ -0,0 +1,9 @@
+export class CZooAnimal {
+    constructor(id, nombre, jaula, tipo, peso) {
+        this.IdAnimal = id;
+        this.nombre = nombre;
+        this.JaulaNumero = jaula;
+        this.IdTypeAnimal = tipo;
+        this.peso = peso;
+    }
+}

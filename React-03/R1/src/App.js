ff580cfc46d0ece9d9305d3435c5dbe77b7c5b15
Santiago <sennsantiago7@gmail.com>
2025-08-14 15:20:52 -0300

    Agregar proyecto Hola Mundo en R1

---
+++ React-03/R1/src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import HolaMundo from "./components/HolaMundo";
+
+function App() {
+  return (
+    <div>
+      <HolaMundo />
+    </div>
+  );
+}
+
+export default App;
---
+++ React-03/R1/src/components/HolaMundo.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import "./HolaMundo.css";
+
+function HolaMundo() {
+  return <h1 className="titulo">Hola, mundo!</h1>;
+}
+
+export default HolaMundo;
